f6119ffabdd107bcd8771ae72b24bf540f882cbd
unknown <soccer_Matt_4@yahoo.com>
2019-05-30 22:32:03 -0500

    Fixed part of code for GetAnimalById to be able to actually find the animal searched for.

---
+++ SQLQuery1.sql
@@ -0,0 +1,40 @@
+INSERT INTO Categories VALUES('Lion')
+INSERT INTO Categories VALUES('Monkey')
+INSERT INTO Categories VALUES('Dog')
+INSERT INTO Categories VALUES('Parrot')
+INSERT INTO Categories VALUES('Cheetah')
+
+INSERT INTO DietPlans VALUES('Meats', 'meat', 15);
+INSERT INTO DietPlans VALUES('Fruits', 'fruits', 5);
+INSERT INTO DietPlans VALUES('Dog', 'dogfood', 1 );
+INSERT INTO DietPlans VALUES('Omnivore', 'meat/veg', .25 );
+INSERT INTO DietPlans VALUES('Gazelles' ,'gazelles', 62 );
+
+INSERT INTO Employees VALUES('Peter', 'Parker', 'Spiderman', 'idontfeelgood', 12, 'wcoslinger@webs.com');
+INSERT INTO Employees VALUES('Tony', 'Stark', 'Ironman', 'iamironman', 34, 'smartestavenger@stark.com');
+INSERT INTO Employees VALUES('Steve', 'Rodgers', 'Captain America', 'avengerassemble', 56, 'supersoldier@usa.com');
+INSERT INTO Employees VALUES('Steven', 'Strange', 'Doctor Strange', 'onlyone', 78, 'magicsurgeon@scalple.com');
+INSERT INTO Employees VALUES('Natasha', 'Romanev', 'Black Widow', 'heybigguy', 90, 'watchmework@guns.com');
+ 
+INSERT INTO Animals VALUES('Lion', 280 , 11, 'Aggressive', 'false', 'false', 'Male', 'available', 1,1,1);
+INSERT INTO Animals VALUES('Monkey', 17 , 7, 'Devious', 'true', 'true', 'Female', 'available', 2,2,2);
+INSERT INTO Animals VALUES('Dog', 45 , 1, 'Playful', 'true', 'true', 'Male', 'available', 3,3,3);
+INSERT INTO Animals VALUES('Parrot', 14, 9, 'Talkative', 'true', 'true', 'Female', 'available', 4,4,4);
+INSERT INTO Animals VALUES('Cheetah', 110 , 13, 'aggressive', 'false', 'false', 'Male', 'available', 5,5,5);
+ 
+INSERT INTO Clients VALUES('Rocket', 'Racoon', 'rocket1', 'illgetthatarm', null, 'trashpanda@stolen.com');
+INSERT INTO Clients VALUES('Groot', 'Groot', 'iamgroot', 'iamgroot', null, 'iamgroot@groot.com');
+INSERT INTO Clients VALUES('Peter', 'Quill', 'starlord', 'itisourpod', null, 'imstarlord@god.com');
+INSERT INTO Clients VALUES('Bucky', 'Barnes', 'wintersoldier', 'gunisnotforsale', null, 'igottheguns@guns.com');
+INSERT INTO Clients VALUES('Drax', 'Destroyer', 'invisible', 'iamincrediblyhumble', null, 'invisible@humble.com');
+ 
+INSERT INTO Rooms VALUES( 1, 1);
+INSERT INTO Rooms VALUES( 2, 2);
+INSERT INTO Rooms VALUES( 3, 3);
+INSERT INTO Rooms VALUES( 4, 4);
+INSERT INTO Rooms VALUES( 5, 5);
+INSERT INTO Rooms VALUES( 6, null);
+INSERT INTO Rooms VALUES( 7, null);
+INSERT INTO Rooms VALUES( 8, null);
+INSERT INTO Rooms VALUES( 9, null);
+INSERT INTO Rooms VALUES( 10, null);
